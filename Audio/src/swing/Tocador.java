44457b618fd32ffc7eccfa982656a6b558d29b66
Charles <Charles@Fortress_of_Solitude>
2013-03-09 20:50:07 -0300

    Criada primeira tela para testes

---
+++ Audio/src/swing/Tocador.java
@@ -0,0 +1,38 @@
+package swing;
+
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import swing.action.JTocarMP3;
+
+@SuppressWarnings("serial")
+public class Tocador extends JFrame {
+	
+	public Tocador(){
+		super("S.T.A.R");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JMenuBar menubar = new JMenuBar();
+        JMenu file = new JMenu("Midia");
+        menubar.add(file);
+        setJMenuBar(menubar);
+        
+        Action tocarAction = new JTocarMP3("C:/Users/Charles/Documents/eclipse/Audio/Victory.mp3");
+		file.add(tocarAction);
+	}
+
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Tocador play = new Tocador();
+				play.setVisible(true);
+			}
+		});
+	}
+
+}
---
+++ Audio/src/swing/action/JTocarMP3.java
@@ -0,0 +1,32 @@
+package swing.action;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.*;
+
+import tocar.MP3;
+
+@SuppressWarnings("serial")
+public class JTocarMP3 extends AbstractAction {
+
+	private File mp3File;
+	private MP3 musica;
+	
+	public JTocarMP3(String path) {
+		super("mp3");
+	    this.mp3File = new File(path);
+	    this.musica = new MP3(mp3File);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		new Thread() {
+            public void run() {
+                try { musica.play(); }
+                catch (Exception e) { System.out.println(e); }
+            }
+        }.start();
+        
+        System.out.println(mp3File.getName());
+	}
+}
