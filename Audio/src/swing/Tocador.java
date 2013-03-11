145846e46829ab20c6b2210eb4b050d11d028907
Charles <Charles@Fortress_of_Solitude>
2013-03-10 21:59:37 -0300

    organizando o código

--- Audio/src/swing/Tocador.java
+++ Audio/src/swing/Tocador.java
@@ -1,6 +1,7 @@
 package swing;
 
 import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -9,23 +10,27 @@ import swing.action.JTocarMP3;
 
 @SuppressWarnings("serial")
 public class Tocador extends JFrame {
-	
-	public Tocador(){
+
+	public Tocador() {
 		super("S.T.A.R");
 		setSize(300, 200);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+        setIconImage(new ImageIcon("star.png").getImage());
+
 		JMenuBar menubar = new JMenuBar();
-        JMenu file = new JMenu("Midia");
-        menubar.add(file);
-        setJMenuBar(menubar);
-        
-        Action tocarAction = new JTocarMP3("C:/Users/Charles/Documents/eclipse/Audio/Victory.mp3");
+		JMenu file = new JMenu("Midia");
+		menubar.add(file);
+		setJMenuBar(menubar);
+
+		/*Action tocarAction = new JTocarMP3(
+				"C:/Users/Charles/Documents/eclipse/Audio/Victory.mp3");*/
+		Action tocarAction = new JTocarMP3(
+				"C:/Users/Charles/Documents/eclipse/Audio/Oasis - Falling Down.mp3");
 		file.add(tocarAction);
 	}
 
-	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
--- Audio/src/swing/action/JTocarMP3.java
+++ Audio/src/swing/action/JTocarMP3.java
@@ -12,21 +12,25 @@ public class JTocarMP3 extends AbstractAction {
 
 	private File mp3File;
 	private MP3 musica;
-	
+
 	public JTocarMP3(String path) {
 		super("mp3");
-	    this.mp3File = new File(path);
-	    this.musica = new MP3(mp3File);
+		this.mp3File = new File(path);
+		this.musica = new MP3(mp3File);
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		new Thread() {
-            public void run() {
-                try { musica.play(); }
-                catch (Exception e) { System.out.println(e); }
-            }
-        }.start();
-        
-        System.out.println(mp3File.getName());
+			public void run() {
+				try {
+					musica.play();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		}.start();
+		
+		System.out.println(mp3File.getName());
 	}
 }
